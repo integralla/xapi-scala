8a8935e31bcebc384fb39c8c0489e4ed2a343295
Andrew Kirk <andrew.kirk@icloud.com>
2023-02-17 11:04:14 -0700

    Add Scaladoc

--- integralla-lrs-model-circe/src/main/scala/io/integralla/model/xapi/statement/identifiers/IRI.scala
+++ integralla-lrs-model-circe/src/main/scala/io/integralla/model/xapi/statement/identifiers/IRI.scala
@@ -30,21 +30,38 @@ case class IRI(value: String) extends StatementValidation with Equivalence {
     }
   }
 
+  /** Generates a signature for what the object logically represents
+    *
+    * As per the xAPI specification, when comparing IRIs, LRSs SHOULD* handle them
+    * only by using one or more of the approaches described in 5.3.1 (Simple String
+    * Comparison) and 5.3.2 (Syntax-Based Normalization) of RFC 3987, and SHOULD*
+    * NOT handle them using any approaches described in 5.3.3 (Scheme-Based
+    * Normalization) or 5.3.4 (Protocol-Based Normalization) of the same RFC, or any
+    * other approaches.
+    *
+    * Here, we lower case both the schema and host name (if present) and sort the
+    * query string parameters.
+    *
+    * By default, `scala-uri` will URL percent encode paths and query string parameters
+    * upon parsing. We do nothing to override this behavior.
+    *
+    *  @return A string identifier
+    */
   override protected[statement] def signature(): String = {
     hash {
       Url
         .parseOption(value).map(url => {
-        Url(
-          scheme = url.schemeOption.map(lower).orNull,
-          user = url.user.orNull,
-          password = url.password.orNull,
-          host = url.hostOption.map(host => lower(host.value)).orNull,
-          port = url.port.getOrElse(-1),
-          path = url.path.toString(),
-          query = QueryString.apply(url.query.params.sorted),
-          fragment = url.fragment.orNull
-        ).toString()
-      }).getOrElse(value)
+          Url(
+            scheme = url.schemeOption.map(lower).orNull,
+            user = url.user.orNull,
+            password = url.password.orNull,
+            host = url.hostOption.map(host => lower(host.value)).orNull,
+            port = url.port.getOrElse(-1),
+            path = url.path.toString(),
+            query = QueryString.apply(url.query.params.sorted),
+            fragment = url.fragment.orNull
+          ).toString()
+        }).getOrElse(value)
     }
   }
 }
