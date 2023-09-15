12161d41b1e771dc16c2d0dcdfca6a02150cd878
Andrew Kirk <andrew.kirk@integralla.io>
2023-09-15 09:38:49 -0600

    Improve StatementContext tests

--- src/test/scala/io/integralla/model/xapi/statement/StatementContextTest.scala
+++ src/test/scala/io/integralla/model/xapi/statement/StatementContextTest.scala
@@ -9,100 +9,100 @@ import io.integralla.model.xapi.statement.identifiers.{Account, IRI, MBox}
 import io.integralla.testing.spec.UnitSpec
 
 import java.util.UUID
-import scala.io.Source
-import scala.util.Using
 
 class StatementContextTest extends UnitSpec {
 
-  val sampleInstructor: Agent = Agent(
-    Some(StatementObjectType.Agent),
-    Some("Andrew Downes"),
-    None,
-    None,
-    None,
-    Some(Account("http://www.example.com", "13936749"))
-  )
-  val sampleTeam: Group =
-    Group(StatementObjectType.Group, Some("Team PB"), Some(MBox("mailto:teampb@example.com")), None, None, None, None)
-
-  val sampleParent: List[Activity] = List(
-    Activity(Some(StatementObjectType.Activity), IRI("http://www.example.com/meetings/series/267"), None)
-  )
-
-  val sampleCategory: List[Activity] = List(
-    Activity(
-      Some(StatementObjectType.Activity),
-      IRI("http://www.example.com/meetings/categories/teammeeting"),
-      Some(
-        ActivityDefinition(
-          Some(LanguageMap(Map("en" -> "team meeting"))),
-          Some(LanguageMap(Map("en" -> "A category of meeting used for regular team meetings."))),
-          Some(IRI("http://example.com/expapi/activities/meetingcategory")),
-          None,
-          None,
-          None,
-          None,
-          None,
-          None,
-          None,
-          None,
-          None
+  val sampleContext: StatementContext = StatementContext(
+    registration = Some(UUID.fromString("1fc289d4-5061-4635-8500-ada2009fb7c8")),
+    instructor =
+      Some(Agent(objectType = Some(StatementObjectType.Agent), mbox = Some(MBox("mailto:instructor@integralla.io")))),
+    team = Some(
+      Group(
+        objectType = StatementObjectType.Group,
+        name = Some("Team A"),
+        mbox = Some(MBox("mailto:team.a@integralla.io")),
+        member = Some(
+          List(
+            Agent(mbox = Some(MBox("mailto:a@integralla.io"))),
+            Agent(mbox = Some(MBox("mailto:b@integralla.io")))
+          )
         )
       )
+    ),
+    contextActivities = Some(
+      ContextActivities(
+        parent = Some(List(Activity(id = IRI("https://lrs.integralla.io/activity/parent")))),
+        grouping = Some(List(Activity(id = IRI("https://lrs.integralla.io/activity/grouping")))),
+        category = Some(List(Activity(id = IRI("https://lrs.integralla.io/activity/category")))),
+        other = Some(List(Activity(id = IRI("https://lrs.integralla.io/activity/other"))))
+      )
+    ),
+    revision = Some("1.0.0"),
+    platform = Some("Integralla"),
+    language = Some("en-UK"),
+    statement = None,
+    extensions = Some(
+      ExtensionMap(Map(IRI("http://id.tincanapi.com/extension/topic") -> "testing".asJson))
     )
   )
-
-  val sampleOther: List[Activity] = List(
-    Activity(Some(StatementObjectType.Activity), IRI("http://www.example.com/meetings/occurances/34257"), None),
-    Activity(Some(StatementObjectType.Activity), IRI("http://www.example.com/meetings/occurances/3425567"), None)
-  )
-
-  val sampleContextActivities: ContextActivities = ContextActivities(
-    Some(sampleParent),
-    None,
-    Some(sampleCategory),
-    Some(sampleOther)
-  )
-
-  val sampleStatementRef: StatementRef =
-    StatementRef(StatementObjectType.StatementRef, UUID.fromString("6690e6c9-3ef0-4ed3-8b37-7f3964730bee"))
-
-  val sampleExtensionValue: Json = Map("name" -> "Kilby", "id" -> "http://example.com/rooms/342").asJson
-  val sampleExtensions: ExtensionMap = ExtensionMap(
-    Map(
-      IRI("http://example.com/profiles/meetings/activitydefinitionextensions/room") -> sampleExtensionValue
-    )
-  )
-
-  val sampleContext: StatementContext = StatementContext(
-    Some(UUID.fromString("ec531277-b57b-4c15-8d91-d292c5b2b8f7")),
-    Some(sampleInstructor),
-    Some(sampleTeam),
-    Some(sampleContextActivities),
-    Some("1.0.0"),
-    Some("Example virtual meeting software"),
-    Some("tlh"),
-    Some(sampleStatementRef),
-    Some(sampleExtensions)
-  )
-
-  val sampleContextEncoded: String = Using.resource(Source.fromResource("data/sample-context-object.json"))(_.mkString)
 
   describe("Statement Context") {
-    describe("[encoding]") {
-      it("should successfully encode a context object") {
-        val actual: String = sampleContext.asJson.spaces2
-        assert(actual === sampleContextEncoded)
-      }
-    }
+    describe("[encoding/decoding]") {
+      it("should successfully encode/decode a context object") {
+        val expected: String =
+          """{
+            |  "registration" : "1fc289d4-5061-4635-8500-ada2009fb7c8",
+            |  "instructor" : {
+            |    "objectType" : "Agent",
+            |    "mbox" : "mailto:instructor@integralla.io"
+            |  },
+            |  "team" : {
+            |    "objectType" : "Group",
+            |    "name" : "Team A",
+            |    "mbox" : "mailto:team.a@integralla.io",
+            |    "member" : [
+            |      {
+            |        "mbox" : "mailto:a@integralla.io"
+            |      },
+            |      {
+            |        "mbox" : "mailto:b@integralla.io"
+            |      }
+            |    ]
+            |  },
+            |  "contextActivities" : {
+            |    "parent" : [
+            |      {
+            |        "id" : "https://lrs.integralla.io/activity/parent"
+            |      }
+            |    ],
+            |    "grouping" : [
+            |      {
+            |        "id" : "https://lrs.integralla.io/activity/grouping"
+            |      }
+            |    ],
+            |    "category" : [
+            |      {
+            |        "id" : "https://lrs.integralla.io/activity/category"
+            |      }
+            |    ],
+            |    "other" : [
+            |      {
+            |        "id" : "https://lrs.integralla.io/activity/other"
+            |      }
+            |    ]
+            |  },
+            |  "revision" : "1.0.0",
+            |  "platform" : "Integralla",
+            |  "language" : "en-UK",
+            |  "extensions" : {
+            |    "http://id.tincanapi.com/extension/topic" : "testing"
+            |  }
+            |}""".stripMargin
 
-    describe("[decoding]") {
-      it("should successfully decode a context object") {
-        val decoded: Either[io.circe.Error, StatementContext] = decode[StatementContext](sampleContextEncoded)
-        decoded match {
-          case Right(actual) => assert(actual === sampleContext)
-          case Left(err)     => throw new Error(s"Decoding failed: $err")
-        }
+        val encoded: String = sampleContext.asJson.spaces2
+        assert(encoded === expected)
+        val decoded: Option[StatementContext] = decode[StatementContext](encoded).toOption
+        assert(decoded.get === sampleContext)
       }
     }
 
@@ -120,13 +120,10 @@ class StatementContextTest extends UnitSpec {
     }
 
     describe("getAgentReferences") {
+
       val group: Group = Group(
         objectType = StatementObjectType.Group,
-        name = None,
-        mbox = None,
         mbox_sha1sum = None,
-        openid = None,
-        account = None,
         member = Some(
           List(
             Agent(None, None, Some(MBox("mailto:picea.engelmannii@integralla.io")), None, None, None),
@@ -135,9 +132,7 @@ class StatementContextTest extends UnitSpec {
         )
       )
 
-      it(
-        "should return a list with a single agent reference if the instructor is an agent, and a team is not defined"
-      ) {
+      it("should return a list of agent references (instructor as agent, no team)") {
         val context: StatementContext = sampleContext.copy(team = None)
         val references: List[AgentReference] = context.getAgentReferences(inSubStatement = false)
         assert(references.length === 1)
@@ -146,15 +141,10 @@ class StatementContextTest extends UnitSpec {
         assert(references.head.asGroupMember === false)
       }
 
-      it(
-        "should return a list with a single agent reference if the instructor is an identified group without members, and a team is not defined"
-      ) {
+      it("should return a list of agent references (instructor as identified group w/o members, no team)") {
         val context: StatementContext = sampleContext.copy(
           instructor = Some(
-            group.copy(
-              mbox = Some(MBox("mailto:instructors@integralla.io")),
-              member = None
-            )
+            group.copy(mbox = Some(MBox("mailto:instructors@integralla.io")), member = None)
           ),
           team = None
         )
@@ -165,9 +155,7 @@ class StatementContextTest extends UnitSpec {
         assert(references.head.asGroupMember === false)
       }
 
-      it(
-        "should return a list with a multiple actors if the instructor is an identified group with members, and a team is not defined"
-      ) {
+      it("should return a list of agent references (instructor as identified group w/ members, no team)") {
         val context: StatementContext = sampleContext.copy(
           instructor = Some(
             group.copy(
@@ -189,9 +177,7 @@ class StatementContextTest extends UnitSpec {
         assert(references.forall(_.inSubStatement === false))
 
       }
-      it(
-        "should return a list with multiple actors if the instructor is an anonymous group with members, and a team is not defined"
-      ) {
+      it("should return a list of agent references (instructor as anonymous group w/ members)") {
         val context: StatementContext = sampleContext.copy(
           instructor = Some(group),
           team = None
@@ -203,20 +189,21 @@ class StatementContextTest extends UnitSpec {
         assert(references.forall(_.asGroupMember === true))
       }
 
-      it("should return a list with multiple actors if the instructor and team are defined (identified group)") {
+      it("should return a list of agent references (instructor as agent, team as identified group)") {
         val context: StatementContext = sampleContext.copy()
         val references: List[AgentReference] = context.getAgentReferences(inSubStatement = false)
-        assert(references.length === 2)
+        assert(references.length === 4)
         assert(references.forall(_.inSubStatement === false))
 
         assert(references.count(_.referenceType === InstructorRef) === 1)
-        assert(references.count(_.referenceType === TeamRef) === 1)
+        assert(references.count(_.referenceType === TeamRef) === 3)
 
         assert(references.filter(_.referenceType === InstructorRef).forall(_.asGroupMember === false))
-        assert(references.filter(_.referenceType === TeamRef).forall(_.asGroupMember === false))
+        assert(references.count(ref => ref.referenceType == TeamRef && !ref.asGroupMember) === 1)
+        assert(references.count(ref => ref.referenceType == TeamRef && ref.asGroupMember) === 2)
       }
 
-      it("should return a list with multiple actors if the instructor and team are defined (anonymous group)") {
+      it("should return a list of agent references (instructor as agent, team as anonymous group)") {
         val context: StatementContext = sampleContext.copy(
           team = Some(group)
         )
