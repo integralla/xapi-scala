ab306ed7594600d5ed793ec16ecef2158b0663d9
Andrew Kirk <andrew.kirk@integralla.io>
2023-10-25 13:05:39 -0600

    * AboutResource versions -> version

--- src/main/scala/io/integralla/model/xapi/about/AboutResource.scala
+++ src/main/scala/io/integralla/model/xapi/about/AboutResource.scala
@@ -2,17 +2,18 @@ package io.integralla.model.xapi.about
 
 import io.circe.{Decoder, Encoder}
 import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
+import io.integralla.model.utils.LRSModel
 import io.integralla.model.xapi.common.ExtensionMap
 
 /** Model for the about resource
   *
-  * @param versions
+  * @param version
   *   A list of versions, composed of the supported latest versions for each
   *   major version
   * @param extensions
   *   A map of other properties, adhering to the format of an extension map
   */
-case class AboutResource(versions: List[String], extensions: Option[ExtensionMap])
+case class AboutResource(version: List[String], extensions: Option[ExtensionMap]) extends LRSModel
 
 object AboutResource {
   implicit val decoder: Decoder[AboutResource] = deriveDecoder[AboutResource]
--- src/test/scala/io/integralla/model/xapi/about/AboutResourceTest.scala
+++ src/test/scala/io/integralla/model/xapi/about/AboutResourceTest.scala
@@ -1,10 +1,10 @@
 package io.integralla.model.xapi.about
 
-import io.integralla.testing.spec.UnitSpec
 import io.circe.syntax.EncoderOps
-import io.circe.jawn.decode
+import io.integralla.model.utils.LRSModel
 import io.integralla.model.xapi.common.ExtensionMap
 import io.integralla.model.xapi.identifiers.IRI
+import io.integralla.testing.spec.UnitSpec
 
 class AboutResourceTest extends UnitSpec {
 
@@ -12,22 +12,22 @@ class AboutResourceTest extends UnitSpec {
     describe("[encoding/decoding]") {
 
       it("should support encoding/decoding json [version only]") {
-        val resource = AboutResource(versions = List("1.0.3"), extensions = None)
-        val encoded: String = resource.asJson.noSpaces
-        val decoded: AboutResource = decode[AboutResource](encoded).toOption.get
-        assert(encoded === """{"versions":["1.0.3"]}""")
+        val resource = AboutResource(version = List("1.0.3"), extensions = None)
+        val encoded: String = resource.toJson[AboutResource]()
+        val decoded: AboutResource = LRSModel[AboutResource](encoded).get
+        assert(encoded === """{"version":["1.0.3"]}""")
         assert(decoded === resource)
       }
 
       it("should support encoding/decoding json [with extensions]") {
         val resource = AboutResource(
-          versions = List("1.0.3", "9274.1.1-2023"),
+          version = List("1.0.3", "2.0.0"),
           extensions = Some(ExtensionMap(Map(IRI("https://lrs.integralla.io/docs") -> "API Documentation".asJson)))
         )
-        val encoded: String = resource.asJson.noSpaces
-        val decoded: AboutResource = decode[AboutResource](encoded).toOption.get
+        val encoded: String = resource.toJson[AboutResource]()
+        val decoded: AboutResource = LRSModel[AboutResource](encoded).get
         assert(
-          encoded === """{"versions":["1.0.3","9274.1.1-2023"],"extensions":{"https://lrs.integralla.io/docs":"API Documentation"}}"""
+          encoded === """{"version":["1.0.3","2.0.0"],"extensions":{"https://lrs.integralla.io/docs":"API Documentation"}}"""
         )
         assert(decoded === resource)
       }
